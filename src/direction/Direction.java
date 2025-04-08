dbcc53cc354b10bc8378bc7d7f2702519178e872
Nyuutrino <74418460+Nyuutrino@users.noreply.github.com>
2025-04-08 15:49:08 -0500

    Add extra documentation for the Direction class

--- src/direction/Direction.java
+++ src/direction/Direction.java
@@ -43,6 +43,10 @@ public class Direction {
 	/**
 	 * Returns the direction to the left of a specified direction (e.g: if you are
 	 * facing north and turn left, you would be facing west)
+	 * 
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as an integer (conforming to the numerical
+	 *         assignments of direction in the Direction class)
 	 */
 	public static int left(int dir) {
 		// The "4 +" is to ensure that the number will wrap around to 0 (e.g: if we're
@@ -55,25 +59,57 @@ public class Direction {
 	 * Returns the direction to the right of a specified direction (e.g: if you are
 	 * facing north and turn right, you would be facing east)
 	 * 
-	 * @param dir
-	 * @return
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as an integer (conforming to the numerical
+	 *         assignments of direction in the Direction class)
 	 */
 	public static int right(int dir) {
-		return (4 + dir + 1) % 4;
+		//No need for the "+ 4" since we are simply adding and not subtracting
+		return (dir + 1) % 4;
 	}
 
 	/**
-	 * Function overload of left() used to take a Direction class instance
+	 * Returns the direction opposite of a specified direction (e.g: if you are
+	 * facing north, the opposite direction would be south)
+	 * 
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as an integer (conforming to the numerical
+	 *         assignments of direction in the Direction class)
+	 */
+	public static int opposite(int dir) {
+		return (dir + 2) % 4;
+	}
+
+	/**
+	 * Function overload of left() used to take a Direction class instance instead
+	 * 
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as a new instance of the Direction class
 	 */
 	public static Direction left(Direction dir) {
 		return new Direction((4 + dir.getDirection() - 1) % 4);
 	}
 
 	/**
-	 * Function overload of right() used to take a Direction class instance
+	 * Function overload of right() used to take a Direction class instance instead
+	 * 
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as a new instance of the Direction class
 	 */
 	public static Direction right(Direction dir) {
-		return new Direction((4 + dir.getDirection() + 1) % 4);
+		return new Direction((dir.getDirection() + 1) % 4);
+	}
+
+	/**
+	 * Function overload of opposite() used to take a Direction class instance
+	 * instead
+	 * 
+	 * @param dir The direction to perform the operation on
+	 * @return The resulting direction as a new instance of the Direction class
+	 */
+
+	public static Direction opposite(Direction dir) {
+		return new Direction((dir.getDirection() + 2) % 4);
 	}
 
 }
