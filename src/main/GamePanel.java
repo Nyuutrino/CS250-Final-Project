fce1bebdb7fbf62c700b75cbb457954116ad124f
Nyuutrino <74418460+Nyuutrino@users.noreply.github.com>
2025-04-14 16:02:13 -0500

    Add the new barrier type for test

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,10 +4,7 @@ import java.awt.*;
 
 import javax.swing.JPanel;
 
-import barrier.Barrier;
-import barrier.Corner;
-import barrier.FourWay;
-import barrier.Hallway;
+import barrier.*;
 import direction.Direction;
 import entity.Player;
 import game.GameConfig;
@@ -33,11 +30,12 @@ public class GamePanel extends JPanel implements Runnable {
 	//Test
 	public Barrier[] barriers;
 	Rectangle playerRect = new Rectangle(player.x, player.y, tileSize, tileSize);
-	Hallway testHall = new Hallway(10, 20, 5, new Direction(Direction.NORTH));
+	Hallway testHall = new Hallway(20, 10, 5, new Direction(Direction.SOUTH));
 	Hallway testHall2 = new Hallway(testHall, testHall.getAvailableNodes()[1], 5);
 	Hallway testHall3 = new Hallway(testHall2, testHall2.getAvailableNodes()[1], 5);
 	Corner testCorner = new Corner(testHall3, testHall3.getAvailableNodes()[1], Corner.LEFT);
 	FourWay testFW = new FourWay(testCorner, testCorner.getAvailableNodes()[1]);
+	ThreeWay testTW = new ThreeWay(testHall, testHall.getAvailableNodes()[0]);
 
 	//set players default position
 	private int playerX = GameConfig.playerX;
@@ -55,7 +53,8 @@ public class GamePanel extends JPanel implements Runnable {
 		testHall2.getAvailableNodes()[1].linkNodes(testHall3.getAvailableNodes()[0]);
 		testHall3.getAvailableNodes()[1].linkNodes(testCorner.getAvailableNodes()[0]);
 		testCorner.getAvailableNodes()[1].linkNodes(testFW.getAvailableNodes()[0]);
-		barriers = new Barrier[]{testHall, testHall2, testHall3, testCorner, testFW};
+		testHall.getAvailableNodes()[0].linkNodes(testTW.getAvailableNodes()[0]);
+		barriers = new Barrier[]{testHall, testHall2, testHall3, testCorner, testFW, testTW};
 	}
 
 	public void startGameThread() {
