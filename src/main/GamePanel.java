5b73d06e996c24f242cd2831d73af7d32bb40748
quaintoctopi <vp2104vx@winona.edu>
2025-04-15 14:51:32 -0500

    Changed while loop to timer system
    commented out collision detection message, this appears to be causing the initial FPS drop
    
    Changed gamePanel to call the modified game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,17 +1,15 @@
 package main;
 
 import java.awt.*;
-
-import javax.swing.JPanel;
-
+import javax.swing.*;
 import barrier.*;
 import direction.Direction;
 import entity.Player;
 import game.GameConfig;
 
-public class GamePanel extends JPanel implements Runnable {
-//SCREEN SETTINGS
+public class GamePanel extends JPanel {
 
+	// SCREEN SETTINGS
 	final int orTileSize = GameConfig.orTileSize;
 	final int scale = GameConfig.scale;
 
@@ -21,15 +19,17 @@ public class GamePanel extends JPanel implements Runnable {
 	final int screenWidth = GameConfig.screenWidth;
 	final int screenHeight = GameConfig.screenHeight;
 
-	//FPS
+	// FPS variables
 	int fps = GameConfig.fps;
+	Timer gameTimer;
+	long lastSecondTime = 0;
 
 	KeyHandler keyH = new KeyHandler();
-	Thread gameThread;
 	Player player = new Player(this, keyH);
-	//Test
+
 	public Barrier[] barriers;
 	Rectangle playerRect = new Rectangle(player.x, player.y, tileSize, tileSize);
+
 	Hallway testHall = new Hallway(20, 10, 5, new Direction(Direction.SOUTH));
 	Hallway testHall2 = new Hallway(testHall, testHall.getAvailableNodes()[1], 5);
 	Hallway testHall3 = new Hallway(testHall2, testHall2.getAvailableNodes()[1], 5);
@@ -37,93 +37,67 @@ public class GamePanel extends JPanel implements Runnable {
 	FourWay testFW = new FourWay(testCorner, testCorner.getAvailableNodes()[1]);
 	ThreeWay testTW = new ThreeWay(testHall, testHall.getAvailableNodes()[0]);
 
-	//set players default position
-	private int playerX = GameConfig.playerX;
-	private int playerY = GameConfig.playerY;
-	private int playerSpeed = GameConfig.playerSpeed;
+	private int drawCount = 0;
+
 
 	public GamePanel() {
-
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
+
+		// Link barrier nodes
 		testHall.getAvailableNodes()[1].linkNodes(testHall2.getAvailableNodes()[0]);
 		testHall2.getAvailableNodes()[1].linkNodes(testHall3.getAvailableNodes()[0]);
 		testHall3.getAvailableNodes()[1].linkNodes(testCorner.getAvailableNodes()[0]);
 		testCorner.getAvailableNodes()[1].linkNodes(testFW.getAvailableNodes()[0]);
 		testHall.getAvailableNodes()[0].linkNodes(testTW.getAvailableNodes()[0]);
+
 		barriers = new Barrier[]{testHall, testHall2, testHall3, testCorner, testFW, testTW};
 	}
+    //game timer loop
+	public void startGameTimer() {
+		int delay = 1000 / fps; // milliseconds delay for 60fps
+		gameTimer = new Timer(delay, e -> {//loop delay
+			update();
+			repaint();
+			drawCount++;
 
-	public void startGameThread() {
-		gameThread = new Thread(this);
-		gameThread.start();
-	}
-
-	@Override
-	public void run() {
-
-		//FPS code
-		double drawInterval = 1000000000 / fps;
-		double delta = 0;
-		long lastTime = System.nanoTime();
-		long currentTime;
-		long timer = 0;
-		int drawCount = 0;
-
-
-		while (gameThread != null) {
-			//System.out.println("game is running");
-
-			currentTime = System.nanoTime();
-
-			//Delta method
-			delta += (currentTime - lastTime) / drawInterval;
-			timer += (currentTime - lastTime);
-			lastTime = currentTime;
-
-			if (delta >= 1) {
-				update();
-				repaint();
-				delta--;
-				drawCount++;
-			}
-
-			if (timer >= 1000000000) {
-//				System.out.println("FPS: " + drawCount);
+			// Optional FPS debug
+			if (System.currentTimeMillis() - lastSecondTime >= 1000) {
+                System.out.println("FPS: " + drawCount);
 				drawCount = 0;
-				timer = 0;
+				lastSecondTime = System.currentTimeMillis();
 			}
-
-		}
-
+		});
+		//start timer
+		gameTimer.start();
 	}
 
 	public void update() {
-
 		player.update();
 		playerRect.x = player.x;
 		playerRect.y = player.y;
 	}
 
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
 		Graphics2D g2 = (Graphics2D) g;
-
 		player.draw(g2);
+		//barrier draw
 		for (Barrier b : barriers) {
 			b.draw(g2);
-			if (b.isColliding(playerRect)) {
-				g2.setColor(Color.WHITE);
-				g2.setFont(new Font("Tahoe", Font.BOLD, 14));
-				g2.drawString("Player is colliding!", 10, 10);
-			}
+			//collision testing
+		//	if (b.isColliding(playerRect)) {
+		//		g2.setColor(Color.WHITE);
+		//		g2.setFont(new Font("Tahoe", Font.BOLD, 14));
+		//		g2.drawString("Player is colliding!", 10, 10);
+		//	}
 		}
 
-		//Debug mode stuff
 		if (GameConfig.debug) {
 			int initialX = 0;
 			int initialY;
@@ -141,7 +115,5 @@ public class GamePanel extends JPanel implements Runnable {
 		}
 
 		g2.dispose();
-
-
 	}
 }
